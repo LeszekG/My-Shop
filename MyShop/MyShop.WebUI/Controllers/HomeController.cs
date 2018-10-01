3f56606c697cdf7713f9d2dda07b9da1f03c0f86
Leszek <Leszek@ELO_ASUS>
2018-10-01 20:53:17 +0200

    10 product listing

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,33 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productContext;
+        IRepository<ProductCategory> productCategoriesContext;
+
+        public HomeController(IRepository<Product> _productContext, IRepository<ProductCategory> _productCategoriesContext)
+        {
+            this.productContext = _productContext;
+            this.productCategoriesContext = _productCategoriesContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<ProductWithCategoryViewModel> pvml = new List<ProductWithCategoryViewModel>();
+            List<Product> products = productContext.Collection().ToList();
+            foreach (Product p in products)
+                pvml.Add(new ProductWithCategoryViewModel(p, productCategoriesContext.Find(p.Category)));
+
+            return View(pvml);
+
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = productContext.Find(Id);
+            if (product == null)
+                return HttpNotFound();
+            else
+                return View(product);
         }
 
         public ActionResult About()
